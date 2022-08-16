3c401c54bff60f89b888bc95383d196cfc2859b3
DAVE.TUSIA@GMAIL.COM~ <DAVE.TUSIA@GMAIL.COM>
2022-08-16 15:45:08 -0700

    Lab completed

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import './HornedBeast.css';
 
 class HornedBeast extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      votes: 0,
+    }
+  }
+
+  handleWaves = () => {
+    this.setState({
+      votes: this.state.votes + 1
+    });
+  }
+
   render() {
     return (
-      <article>
+      <article className="beast">
         <h2>{this.props.title}</h2>
-        <img src={this.props.image} alt={this.props.alt} title={this.props.description}/>
+        <img src={this.props.image_url} alt={this.props.alt} title={this.props.description}/>
+        <p>{this.props.horns}</p>
+        <p>{this.props.keyword}</p>
+        <p>♥️{this.state.votes}</p>
+        <p onClick={this.handleWaves}>Say Hello!</p>
         <p>{this.props.description}</p>
       </article>
     )
--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import HornedBeast from './HornedBeast.js';
+import './Main.css';
+import ListOfBeasts from './data.json';
 
 class Main extends React.Component {
+  
   render() {
+    let newArr = ListOfBeasts.map((beast, idx) => (
+      <HornedBeast
+        key={idx}
+        image_url={beast.image_url}
+        beast={beast.title}
+        description={beast.description}
+        keyword={beast.keyword}
+        horns={beast.horns}/>
+    ))
+
     return (
       <main>
-        <HornedBeast title="UniWhal" image="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" alt="image1" description="A unicorn and a narwhal nuzzling their horns"/>
-        <HornedBeast title="Rhino Family" image="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80" alt="image1" description="Parent rhino with two babies"/>
-        <HornedBeast title="Unicorn Head" image="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg" description="Someone wearing a very silly unicorn head mask"/>
+      {newArr}
       </main>
-    )
+    );
   }
+  
+  
 }
+
 
 export default Main;
