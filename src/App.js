79960969d4982a1606b3ba324094532a88c17a5a
DAVE.TUSIA@GMAIL.COM~ <DAVE.TUSIA@GMAIL.COM>
2022-08-18 09:39:23 -0700

    complete

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Header from './Header.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
 import data from './data.json';
-// import SelectedBeast from './SelectedBeast';
-import Modal from 'react-bootstrap/Modal';
-import { Card } from 'react-bootstrap';
+import SelectedBeast from './SelectedBeast';
+// import Modal from 'react-bootstrap/Modal';
+// import { Card } from 'react-bootstrap';
 
 
 class App extends React.Component {
@@ -39,18 +39,19 @@ class App extends React.Component {
         <Header/>
         <Main data={data} temp={this.handleShowModal}/>
         <Footer/>
-        <Modal
+        <SelectedBeast
+        beast={this.state.beast}
         show={this.state.showModal}
         onHide={this.handleHideModal}
       >
-        <Card className="beast">
+        {/* <Card className="beast">
           <title title={this.state.beast.title}></title>
           <img src={this.state.beast.image_url} alt={this.state.beast.keyword} title={this.state.beast.description}></img>
           <p>{this.state.beast.votes}</p>
           <p>♥️</p>
           <p>{this.state.beast.description}</p>
-        </Card>
-      </Modal>
+        </Card> */}
+      </SelectedBeast>
         
       </>
     )
--- src/SelectedBeast.js
+++ src/SelectedBeast.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import Modal from 'react-bootstrap/Modal';
+import { Card } from 'react-bootstrap';
 
 
 class SelectedBeast extends React.Component {
@@ -7,9 +8,11 @@ class SelectedBeast extends React.Component {
   render() {
     return (
       <Modal
+      show={this.props.show}
+      onHide={this.props.onHide}
       >
         <Card className="beast">
-        <title title={this.props.title}></title>
+        <title title={this.props.beast.title}></title>
           <img src={this.props.beast.image_url} alt={this.props.beast.keyword} title={this.props.beast.description}></img>
           <p>{this.props.beast.votes}</p>
           <p>♥️</p>
